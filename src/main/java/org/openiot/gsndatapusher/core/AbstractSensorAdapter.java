c1035e746acbc52de7c2e1b0b52f392d39c43960
Hylke van der Schaaf <hylke.vanderschaaf@iosb.fraunhofer.de>
2014-04-28 16:30:23 +0200

    * Fields of type int now contain the number of seconds till the next full hour as data.

--- src/main/java/org/openiot/gsndatapusher/core/AbstractSensorAdapter.java
+++ src/main/java/org/openiot/gsndatapusher/core/AbstractSensorAdapter.java
@@ -1,16 +1,21 @@
 package org.openiot.gsndatapusher.core;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Random;
 
 /**
  *
  * @author admin-jacoby
+ * @param <A>
+ * @param <C>
  */
 public abstract class AbstractSensorAdapter<A extends ISensorAdapter<A, C>, C extends ISensorConfig<A, C>> implements ISensorAdapter<A, C> {
 
 	private static final String validCharacters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_";
 	protected Random random;
+	private static long nextHour = 0;
 
 	public AbstractSensorAdapter() {
 		random = new Random();
@@ -26,7 +31,18 @@ public abstract class AbstractSensorAdapter<A extends ISensorAdapter<A, C>, C ex
 		String result = "";
 		switch (config.getFieldType()) {
 			case Int:
-				result = String.valueOf(random.nextInt());
+				long now = System.currentTimeMillis() / 1000;
+				long span = nextHour - now;
+				if (span < 0) {
+					Calendar nextHourCal = Calendar.getInstance();
+					nextHourCal.set(Calendar.MILLISECOND, 0);
+					nextHourCal.set(Calendar.SECOND, 0);
+					nextHourCal.set(Calendar.MINUTE, 0);
+					nextHourCal.add(Calendar.HOUR_OF_DAY, 1);
+					nextHour = nextHourCal.getTimeInMillis() / 1000;
+					span = nextHour - now;
+				}
+				result = String.valueOf(span);
 				break;
 			case Double:
 				result = String.format(Locale.US, "%f", random.nextDouble());
